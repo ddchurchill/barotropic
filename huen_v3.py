eb012d5985033f26946a1a30acea31b2eb22e298
Dean Churchill <deanchurchill@att.net>
2023-05-21 15:59:57 -0700

    removed huen_v2 code

--- huen_v3.py
+++ huen_v3.py
@@ -23,8 +23,9 @@ def huen_v3(dataset, lat0, lon0, timestamps):
     # deltat is the time step in seconds between trajectory nodes.
     deltat = int((timestamps[1] - timestamps[0])/ np.timedelta64(1, 's'))
 
+    # lat0 and lon0 are the starting  point of the trajectory
     trajectory = Trajectory_v2(lat0, lon0, deltat, timestamps[0])
-    # lat1 and lon1 are tracking the start point of the trajectory
+
     lat1 = lat0
     lon1 = lon0  
 # iterate through the timestamps, noting the last one terminates the
@@ -99,74 +100,6 @@ def huen_v3(dataset, lat0, lon0, timestamps):
 ##                                                                                     
     return trajectory
     
-def huen_v2(wind_model, lat0, lon0, deltat, nsteps):
-    """
-    huen - v2 - compute trajectory integrating by huens method                             
-    input:
-	wind_model - method that returns the velocity (m/s)
-        at specified           
-        input latitude, longitude and time step.
-	This function is variable, to accomodate 
-	test functions as well as for production wind data.                               
-    input lat0 - starting latitude of parcel                                          
-    input lon0 - starting longitude of parcel                                         
-    input deltat - time step in seconds.                                              
-    input nsteps - number of time steps to integrate over                                 
-    Returns:                                                                          
-    Trajectory_v2 object            
-    """
-
-    trajectory = Trajectory_v2()
-    # lat1 and lon1 are tracking the start point of the trajectory
-    lat1 = lat0
-    lon1 = lon0  
-
-#
-# repeat for each requested time step
-# adding in a starting point
-#
-    for time in range(0, nsteps +1):
-        wind = wind_model(lat1, lon1, time)
-
-        if wind is np.nan: # no wind found at this location/time
-            break;
-
-        denom = EARTH_RADIUS * np.cos(np.deg2rad(lat1))
-        # lon_bar is the intermediate guess using euler method
-        lon_bar  = lon1 + np.degrees(wind.u * deltat / denom)
-
-        # lat_bar is intermediate guess using euler method
-        lat_bar = lat1 + np.degrees(wind.v * deltat / EARTH_RADIUS )
-        # get updated wind vector at updated longitude and latitude 
-        # and at the next time step.                                
-        wind_bar = wind_model(lat_bar, lon_bar, time + 1)
-
-        if wind_bar is None: #  lat & lon are out of bounds          
-            break;
-
-        # update the longitude with huen's method.
-        # use the euler updated latitude to convert distance to degrees               
-        lon2 = lon1 + \
-            0.5 * deltat *np.degrees((wind.u + wind_bar.u)/ \
-                 (EARTH_RADIUS * np.cos(np.deg2rad(lat_bar))))
-
-        lat2 = lat1 + \
-            0.5 * deltat *np.degrees((wind.v + wind_bar.v) / EARTH_RADIUS)
-
-            
-        dx = lon2 - lon1
-        dy = lat2 - lat1  # used for plotting trajectories
-        # append the updated lat and lon to the trajectory object
-        point = TrajectoryPoint(lat1, lon1, dx, dy, time)
-        trajectory.points.append(point)
-
-        # advance to the next point
-        lat1 = lat2
-        lon1 = lon2 
-#
-## return the trajectory object
-##                                                                                     
-    return trajectory
 
         
 
