da59294fa7da8f88bff553f5f4f8c97dd66e88e4
Dean Churchill <deanchurchill@att.net>
2023-06-14 16:54:53 -0700

    computed vorticity and dumped it

--- dumpz.py
+++ dumpz.py
@@ -3,37 +3,47 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 from traject_constants import * 
-def laplacian(data, dx, dy):
+def laplacian(data, x, y):
     nrows, ncols = data.shape
     laplace = np.zeros((nrows, ncols))
+    dx = np.zeros((nrows, ncols))
+    dy = np.zeros((nrows, ncols))
     print("size: ", nrows, ncols)
     for i in 1, nrows -2:
         for j in 1, ncols -2:
+            dx[i,j] = x[i,j+1] - x[i,j]
+            dy[i,j] = y[i+1,j] - y[i,j]
             d2x =  data[i,j+1] - 2*data[i,j] + data[i,j-1]
             d2y = data[i+1,j] - 2*data[i,j] + data[i-1,j]
-            laplace[i,j] = d2x/dx**2 + d2y/dy**2
+            laplace[i,j] = d2x/dx[i,j]**2 + d2y/dy[i,j]**2
 
-    return laplace
+    return laplace, dx, dy
 
 def get_timestamp(start_time, time_step):
     time_stamp = np.datetime64(start_time + pd.Timedelta(hours=time_step))
     dt_str = np.datetime_as_string(time_stamp, unit='s')
     return dt_str, time_stamp
 
-def print_array(data):
-    print("\t\tLongitude")
-    print("lat ", end="")
+def print_array(title, data):
+    line = "_" * 60
+    print()
+
+    print("\t\t", title)
+    print(line)
+    print("\t\t\tLongitude")
+    print("Lat ", end="")
     for i in range(0,nlon):
-        print(f"\t{lon_lin[i]:6.2f}", end="")
+        print(f"\t{lon_lin[i]:10.2f}", end="")
     print("")
+    print("-"*60)
     for i in range(nlat -1, -1, -1):
             print(lat_lin[i], end="")
             for j in range(0, nlon):
                 d = data[i,j]
-                print(f"\t{d:6.2f}", end=" ")
+                print(f"\t{d:.4e}", end=" ")
             print("")
 
-            
+    print("-"*60)
 
 
 #print("lat_lin: ",lat_lin)
@@ -61,13 +71,11 @@ steps =  dataset.variables['step']
 nsteps = len(steps)
 step = 7 # 7z on 15th 
 
-x = np.cos(np.deg2rad(lat_lin))*EARTH_RADIUS*np.cos(np.deg2rad(lon_lin))
-y = EARTH_RADIUS * np.deg2rad(lat_lin)
-dx = np.diff(x)
-dy = np.diff(y)
-print("dx: ", dx)
-print("dy:", dy)
+x = np.cos(np.deg2rad(lat))*EARTH_RADIUS*lon_rad
+y = EARTH_RADIUS * np.deg2rad(lat)
 #
+print_array('X', x)
+print_array('Y', y)#
 timestamps = dataset["time"].values.copy()
 forecast_init_time = timestamps[0] # 15Sep 2007
 start_time_str, start_time = get_timestamp(forecast_init_time, step)
@@ -85,26 +93,20 @@ z500 = dataset.sel({
 #
 # print a table of data
 #
-#print(z500)
-pp = pprint.PrettyPrinter()
-array = np.around(z500, decimals=2)
-pp.pprint(array)
-
-#print(lat_lin)
-#print(lon_lin)
-rdiff = np.diff(z500,axis=1)
-row_diffs = np.around(np.diff(rdiff, axis=1), decimals=2)
-cdiff = np.diff(z500, axis=0)
-col_diffs = np.around(np.diff(cdiff, axis=0), decimals=2)
-np.set_printoptions(suppress=True)
-
-print(" row diffs:")
-pp.pprint(row_diffs)
-print(" col diffs:")
-pp.pprint(col_diffs)
-zeta = laplacian(z500, 1.,1.)
-print("laplacian:", zeta.shape)
-#pp.pprint(zeta)
-print_array(zeta)
+print_array('z500', z500)
 
 
+zeta, dx, dy  = laplacian(z500, x, y)
+
+print_array("Dx", dx)
+print_array('Dy', dy)
+print_array('Laplacian', zeta)
+print_array('coriolis', f)
+
+# compute vorticity, multiply by gravity, divide by f
+vorticity = zeta * GRAVITY / f
+print_array('vorticity', vorticity)
+f_35deg = 2. * OMEGA * np.sin(np.deg2rad(35.))
+print("Omega: ", OMEGA)
+print("f 35 deg: ", f_35deg)
+                           
